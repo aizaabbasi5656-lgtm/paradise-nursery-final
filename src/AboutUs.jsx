7139c04db6cf2a92209970880eb553085df1634d
aizaabbasi5656-lgtm <aizaabbasi5656@gmail.com>
2026-08-31 12:54:02 +0500

    Create AboutUs.jsx

---
+++ src/AboutUs.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function AboutUs() {
+  return (
+    <div className="about-us-container">
+      <h2>About Us</h2>
+      <p>
+        Welcome to Paradise Nursery, your ultimate destination for indoor greenery. We believe that plants bring life, health, and tranquility into every home. Our mission is to curate the finest collection of houseplants, making it easy for plant enthusiasts to discover, learn about, and nurture their favorite green companions.
+      </p>
+    </div>
+  );
+}
+
+export default AboutUs;
